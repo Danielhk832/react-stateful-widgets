3223a5ece76794cc65cbc43c822ef19412fc73ae
Daniel Kish <daniel@Daniels-Mac-mini.local>
2021-09-07 10:15:14 +0700

    finished through programmers. only spinner and squares remain

--- src/components/Input.js
+++ src/components/Input.js
@@ -44,19 +44,18 @@ export default function Input() {
   const changeInput = evt => {
     // When the input changes, its whole value can be found inside the event object.
     // Log out the synthetic event object 'evt' and see for yourself.
-    console.log(evt);
     const { value } = evt.target;
-
+    setInputValue(value);
     /* STEP 4 */
   };
   const reset = () => {
-    /* STEP 5 */
+    setInputValue('')
   };
 
   const style = {
     fontSize: '1.5em',
     marginBottom: '0.3em',
-    color: ((inputValue.length > 10) ? 'crison' : 'royalblue'),
+    color: ((inputValue.length > 10) ? 'crimson' : 'royalblue'),
   };
 
   return (
@@ -64,7 +63,7 @@ export default function Input() {
       <h2>Input</h2>
       <div id='output' style={style}>{inputValue.toUpperCase()}</div> {/* STEP 3 */}
       <div>
-        <input id='input' type='text' onChange={changeInput} /> {/* STEP 6 */}
+        <input id='input' type='text' onChange={changeInput} value={inputValue} /> {/* STEP 6 */}
         <button id='resetInput' onClick={reset}>Reset</button>
       </div>
     </div>
